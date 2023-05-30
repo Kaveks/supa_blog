3665828c8ac0f0edd35f0364448316cc6d919498
Patrick Mbugua <pkaveks2@gmail.com>
2023-05-30 18:53:12 +0000

    updated login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,67 +3,80 @@ import supabase from "@/app/config/supabaseClient";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+const LoginPage = async () => {
+  const router = useRouter();
+  const [userAuth, setUserAuth] = useState('');
 
-const LoginPage = async() => {
-    const router= useRouter()
-        
-    async function signInWithGoogle() {
+  useEffect(() => {
+    const signInWithGoogle = async () => {
+      try {
         const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: 'google',
-        options: {
+          provider: 'google',
+          options: {
             queryParams: {
-            access_type: 'offline',
-            prompt: 'consent',
-            emailRedirectTo: `${location.origin}/auth/callback`,
+              access_type: 'offline',
+              prompt: 'consent',
+              emailRedirectTo: `${location.origin}/auth/callback`,
             },
-        },
-        })
-        router.refresh()
-        console.log(data)
-    }
-    
-    async function signout() {
-        const { error } = await supabase.auth.signOut()
-        router.refresh
+          },
+        });
+        if (error) {
+          setUserAuth("Sorry we could not authenticate your google login, try again later!");
+        } else {
+          setUserAuth(data.toString());
+          console.log(data);
+        }
+      } catch (error) {
+        setUserAuth("Sorry we could not authenticate your google login, try again later!");
       }
-     
-      
-    return ( 
-        <>
-        <main className=" max-w-contentContainer h-screen mdl:h-full mx-auto my-2 md:my-28 flex flex-col bg-teal-600">
-          <div className="w-full h-screen items-center mx-auto flex flex-col">
-          <div 
-          className=" border-2 tracking-wide w-full h-screen my-auto mx-auto gap-2  
-          mdl:w-[600px] mdl:h-[400px] justify-center items-center font-bodyFont 
-          flex flex-col bg-red-300 rounded-lg">
-    
-              <button 
-              onClick={signInWithGoogle}
-              className="border-2 font-md text-base inline-flex mdl:text-2xl mdl:font-semibold shadow-md 
-              mdl:w-[300px] bg-green-400 rounded-lg px-2 max-w-[300px] items-center justify-center my-2 mdl:mb-8 
-              ">
-                <span className="text-yellow-500 mr-2 inline-flex items-center justify-center">
-                  <FontAwesomeIcon icon={faGoogle} style={{height:'18px'}} /> </span>
-              Login with Google</button>
-    
+    };
 
-                <div className="items-center gap-2 mt-4 mb-2 font-titleFont text-medium font-semibold
-                 text-black justify-center flex flex-row sm:flex-col">
-                  <button className=' inline-flex order-1 sm:w-[80px] rounded-md items-center justify-center hover:bg-yellow-100 hover:border-4
-                   hover:text-zinc-600 bg-teal-400 border-2 duration-300'
-                  onClick={signInWithGoogle}>Sign in</button>
-                  <button className=' inline-flex order-3 sm:w-[80px] items-center justify-center rounded-md hover:bg-yellow-100 hover:border-4
-                   hover:text-zinc-600 bg-teal-400 border-2 duration-300' 
-                  onClick={signout}>Sign out</button>
-                </div>
+    const signout = async () => {
+      const { error } = await supabase.auth.signOut();
+    };
+
+    signInWithGoogle();
+  }, []);
+
+  return (
+    <>
+      <main className="max-w-contentContainer h-screen mdl:h-full mx-auto my-2 md:my-28 flex flex-col bg-teal-600">
+        <div className="w-full h-screen items-center mx-auto flex flex-col">
+          <div className="border-2 tracking-wide w-full h-screen my-auto mx-auto gap-2 mdl:w-[600px] mdl:h-[400px] justify-center items-center font-bodyFont flex flex-col bg-red-300 rounded-lg">
+            <button
+              onClick={signInWithGoogle}
+              className="border-2 font-md text-base inline-flex mdl:text-2xl mdl:font-semibold shadow-md mdl:w-[300px] bg-green-400 rounded-lg px-2 max-w-[300px] items-center justify-center my-2 mdl:mb-8"
+            >
+              <span className="text-yellow-500 mr-2 inline-flex items-center justify-center">
+                <FontAwesomeIcon icon={faGoogle} style={{ height: '18px' }} />
+              </span>
+              Login with Google
+            </button>
+
+            <div className="items-center gap-2 mt-4 mb-2 font-titleFont text-medium font-semibold text-black justify-center flex flex-row sm:flex-col">
+              <button
+                className='inline-flex order-1 sm:w-[80px] rounded-md items-center justify-center hover:bg-yellow-100 hover:border-4 hover:text-zinc-600 bg-teal-400 border-2 duration-300'
+                onClick={signInWithGoogle}
+              >
+                Sign in
+              </button>
+              <button
+                className='inline-flex order-3 sm:w-[80px] items-center justify-center rounded-md hover:bg-yellow-100 hover:border-4 hover:text-zinc-600 bg-teal-400 border-2 duration-300'
+                onClick={signout}
+              >
+                Sign out
+              </button>
+            </div>
           </div>
-          </div>
-        </main>
-        </>
-     );
-}
- 
+        </div>
+      </main>
+    </>
+  );
+};
+
 export default LoginPage;
+
 
 
 
